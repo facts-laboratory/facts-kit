d7c65c8100dd56eed74fd98e71800652feec1a13
Justin Shaw <mogulx_operates@yahoo.com>
2023-03-31 17:02:25 -0500

    feat: sell

--- packages/facts-sdk/src/lib/api/position/sell.ts
+++ packages/facts-sdk/src/lib/api/position/sell.ts
@@ -1,15 +1,21 @@
-/* eslint-disable no-useless-escape */
 import { SellInput, State } from '../../faces/state';
-import { payFactMarket } from './pay-fact-market';
-import { getPrice } from './get-price';
+import { getReturns } from './get-price';
 import { interact, pipeP } from './interact';
-import { getEdges } from '@facts-kit/contract-kit';
 
 export async function sell(input: {
   funcInput: Partial<SellInput>;
   contract: string;
   state?: State;
 }) {
-  const fn = pipeP([getPrice, payFactMarket, interact]);
-  return fn(input);
+  const { funcInput, contract, state } = input;
+  const fn = pipeP([getReturns, interact]);
+  return fn({
+    funcInput: {
+      ...(funcInput as SellInput),
+    },
+    state,
+    positionType: funcInput.positionType as 'support' | 'oppose',
+    contract,
+    qty: funcInput.qty as number,
+  });
 }
