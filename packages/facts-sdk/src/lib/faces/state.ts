c41e6f4390d80b9c9c0aaa75c49c1482bb73cdb2
Justin Shaw <mogulx_operates@yahoo.com>
2023-03-31 17:06:09 -0500

    refactor: updated Fact Market state

--- packages/facts-sdk/src/lib/faces/state.ts
+++ packages/facts-sdk/src/lib/faces/state.ts
@@ -1,26 +1,25 @@
 export interface State {
-  pf: number;
-  author: number;
+  creator_cut: number;
+  facts: number;
   pair: string;
   creator: string;
   name?: string;
-  price: number;
   balances: {
     [address: string]: number;
   };
   oppositionBalances: {
     [address: string]: number;
   };
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   canEvolve: boolean;
   evolve: string | null;
+  position: 'support' | 'oppose';
 }
-export interface DmmAction {
-  input: DmmInput;
+export interface FactMarketAction {
+  input: FactMarketInput;
   caller: string;
 }
-export interface DmmInput {
-  function: DmmFunction;
+export interface FactMarketInput {
+  function: FactMarketFunction;
   target?: string;
   qty?: number;
   tx?: string;
@@ -41,21 +40,13 @@ export interface BuyInput {
   txId: string;
 }
 
-export interface DistributeAction {
-  input: DistributeInput;
+export interface WithdrawAction {
+  input: WithdrawInput;
   caller: string;
 }
 
-export interface DistributeInput {
-  function: 'distribute';
-}
-export interface AuthorDistributeAction {
-  input: AuthorDistributeInput;
-  caller: string;
-}
-
-export interface AuthorDistributeInput {
-  function: 'authorDistribute';
+export interface WithdrawInput {
+  function: 'withdraw';
 }
 export interface SellAction {
   input: SellInput;
@@ -84,36 +75,9 @@ export interface Result {
   balance: number;
 }
 
-export type DmmFunction =
-  | 'buy'
-  | 'getPrice'
-  | 'sell'
-  | 'transfer'
-  | 'set-content'
-  | 'balance'
-  | 'content-id'
-  | 'allow'
-  | 'claim'
-  | 'test'
-  | 'distribute'
-  | 'returnUnused'
-  | 'distributeRefundables'
-  | 'processRefundables';
+export type FactMarketFunction = 'buy' | 'sell' | 'withdraw' | 'evolve';
 
 export type ContractResult =
   | { state: State }
   | { result: Result }
-  | { price: number; fee: number }
-  | { result: { contentTxId: string } };
-
-export const initialState: State = {
-  creator: '',
-  pf: 0,
-  author: 0,
-  balances: {},
-  oppositionBalances: {},
-  price: 1,
-  pair: 'VFr3Bk-uM-motpNNkkFg4lNW1BMmSfzqsVO551Ho4hA',
-  canEvolve: true,
-  evolve: null,
-};
+  | { price: number; fee: number };
