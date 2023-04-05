b66b6eda9c41b0d4fdc74ddb851356882b3a7dc6
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-04 20:09:12 -0500

    fix: use good query for player rebuttals

--- packages/query-kit/src/lib/queries/get-player-rebuttals-amount.ts
+++ packages/query-kit/src/lib/queries/get-player-rebuttals-amount.ts
@@ -13,9 +13,9 @@ export async function getPlayerRebuttalsAmount(
       'accept-language': 'en-US,en;q=0.7',
       'content-type': 'application/json',
     },
-    body: `{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  transactions(first: 100, tags: [{name: \\\"Fact-Rebuts\\\", values: [\\\"${tx}\\\"]}]) {\\n    edges {\\n      node {\\n        id\\n        owner {\\n          address\\n        }\\n        tags {\\n          name\\n          value\\n        }\\n      }\\n    }\\n  }\\n}\\n\"}`,
+    body: `{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  transactions(\\n    first: 100\\n    owners: [\\\"${tx}\\\"]\\n    tags: [{name: \\\"Permafacts-Type\\\", values: [\\\"Rebuttal\\\"]}]\\n  ) {\\n    edges {\\n      node {\\n        id\\n      }\\n    }\\n  }\\n}\\n\"}`,
     method: 'POST',
   });
-  const edges = getEdges(await res.json()).map(getNode);
-  return edges;
+  const nodes = getEdges(await res.json()).map(getNode);
+  return nodes;
 }
