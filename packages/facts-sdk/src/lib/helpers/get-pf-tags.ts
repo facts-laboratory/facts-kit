3e98805998ac9692e4f0c405b418d039dcd9a26c
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-17 10:28:58 -0500

    fix: add Type tag as default

--- packages/facts-sdk/src/lib/helpers/get-pf-tags.ts
+++ packages/facts-sdk/src/lib/helpers/get-pf-tags.ts
@@ -1,14 +1,14 @@
-import * as packageJson from '../../../package.json';
 export const FACT_MARKET_SRC = 'ZHXEHvTFbklHB1eV9PFsHc12QZnf09creVHtWZAl64g';
 
 export function getPermafactsTags() {
   return [
     {
       name: 'Permafacts-Type',
-      value: `Assertion-Alpha-v2-${packageJson.version}`,
+      value: `Assertion`,
     },
     { name: 'Permafacts-Version', value: 'Alpha-2' },
     { name: 'Contract-Src', value: FACT_MARKET_SRC },
     { name: 'Content-Type', value: 'text/plain' },
+    { name: 'Type', value: 'fact-post' },
   ];
 }
