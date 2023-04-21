8efe84cf5fdca7d0b2227b7f87d9a1ac5c62c39e
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-21 12:29:56 -0500

    docs: update comment

--- packages/facts-sdk/src/index.ts
+++ packages/facts-sdk/src/index.ts
@@ -8,5 +8,5 @@ export * from './lib/faces/';
 export * as query from './lib/query';
 export { FACTS_KIT_VERSION } from './lib/helpers/get-pf-version';
 export { newReadState } from './lib/helpers/read-state';
-// No one should ever do this
+// No one should ever do this (jshaw is fired)
 export * as price from './lib/api/position/get-price';
