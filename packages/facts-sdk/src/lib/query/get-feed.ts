222d9a1e08a90288d5d0c1aa193347b89db13e17
Luke <lukeannison@gmail.com>
2023-04-28 15:43:35 +0100

    feat: cursor in query

--- packages/facts-sdk/src/lib/query/get-feed.ts
+++ packages/facts-sdk/src/lib/query/get-feed.ts
@@ -3,29 +3,36 @@ import { getEdges, getNode } from '@facts-kit/contract-kit';
 import { Transaction } from '@facts-kit/faces';
 
 export async function getFeed(
-  category?: string
-  // TODO: setup cursor func
-  // cursor?: string
-): Promise<Transaction[]> {
-  const res = category
-    ? await fetch('https://arweave.net/graphql', {
-        headers: {
-          accept: '*/*',
-          'accept-language': 'en-US,en;q=0.7',
-          'content-type': 'application/json',
-        },
-        body: `{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  transactions(\\n    first: 30\\n    tags: [{name: \\\"Topic:${category}\\\", values: [\\\"${category}\\\"]}, {name: \\\"Type\\\", values: [\\\"meme\\\", \\\"fact-post\\\"]}, {name: \\\"Permafacts-Version\\\", values: [\\\"Alpha-2\\\"]}]\\n  ) {\\n    edges {\\n      node {\\n        id\\n      }\\n    }\\n  }\\n}\\n\"}`,
-        method: 'POST',
+    category?: string,
+    cursor?: string
+  ): Promise<Transaction[]> {
+    const res = category
+      ? await fetch('https://arweave.net/graphql', {
+          headers: {
+            accept: '*/*',
+            'accept-language': 'en-US,en;q=0.7',
+            'content-type': 'application/json',
+          },
+  
+          body: `{"operationName":null,"variables":{},"query":"{\\n  transactions(\\n    first: 30${cursor ? `, after: \\"${cursor}\\"` : ''}\\n    tags: [{name: \\"Permafacts-Version\\", values: [\\"Alpha-2\\"]}]\\n  ) {\\n    edges {\\n      cursor \\n      node {\\n        id\\n        owner {\\n          address\\n        }\\n        block {\\n          timestamp\\n          height\\n        }\\n        tags {\\n          name\\n          value\\n        }\\n      }\\n    }\\n  }\\n}\\n"}`,
+          method: 'POST',
+        })
+      : await fetch('https://arweave.net/graphql', {
+          headers: {
+            accept: '*/*',
+            'accept-language': 'en-US,en;q=0.7',
+            'content-type': 'application/json',
+          },
+          body: `{"operationName":null,"variables":{},"query":"{\\n  transactions(\\n    first: 30${cursor ? `, after: \\"${cursor}\\"` : ''}\\n    tags: [{name: \\"Permafacts-Version\\", values: [\\"Alpha-2\\"]}]\\n  ) {\\n    edges {\\n      cursor \\n      node {\\n        id\\n        owner {\\n          address\\n        }\\n        block {\\n          timestamp\\n          height\\n        }\\n        tags {\\n          name\\n          value\\n        }\\n      }\\n    }\\n  }\\n}\\n"}`,
+          method: 'POST',
+        });
+    const edges = getEdges(await res.json());
+    console.log("edges", edges);
+    const formattedEdges = edges.map(
+      ({ cursor, node }: { cursor: string; node: Transaction }) => ({
+        cursor,
+        ...node,
       })
-    : await fetch('https://arweave.net/graphql', {
-        headers: {
-          accept: '*/*',
-          'accept-language': 'en-US,en;q=0.7',
-          'content-type': 'application/json',
-        },
-        body: `{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  transactions(\\n    first: 30\\n    tags: [{name: \\\"Permafacts-Version\\\", values: [\\\"Alpha-2\\\"]}]\\n  ) {\\n    edges {\\n      node {\\n        id\\n        owner {\\n          address\\n        }\\n        block {\\n          timestamp\\n          height\\n        }\\n        tags {\\n          name\\n          value\\n        }\\n      }\\n    }\\n  }\\n}\\n\"}`,
-        method: 'POST',
-      });
-  const edges = getEdges(await res.json()).map(getNode);
-  return edges;
-}
+    );
+    return formattedEdges;
+  }
