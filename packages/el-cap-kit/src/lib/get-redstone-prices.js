0c675402710f799820a3b0c7f33050ac940f81f3
Luke <lukeannison@gmail.com>
2023-05-22 19:22:07 +0100

    feat: first item from each call being combined and returned
    the logic for extracting the first item can be removed to give a full combined list from each call

--- packages/el-cap-kit/src/lib/get-redstone-prices.js
+++ packages/el-cap-kit/src/lib/get-redstone-prices.js
@@ -1,31 +1,22 @@
-/* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import Async from 'hyper-async';
-const { fromPromise, of, Resolved, Rejected } = Async;
+const { fromPromise, of } = Async;
 
-export function getPrices(prices) {
+export function getPrices(input) {
   return (
-    of(prices)
-      // validate
-      // side-effect close to edges
-      .chain((prices) => {
-        if (prices) return Resolved('We have prices');
-        return Rejected('no prices');
+    of(input)
+      .chain((input) => fromPromise(getRedstonePrices)(input))
+      .map((input) => {
+        console.log('Redstone Result:', input);
+        return input;
       })
-      .chain((prices) => {
-        if (prices) return Resolved('We have prices');
-        return Rejected('no prices');
-      })
-      .chain((prices) => {
-        if (prices) return Resolved('We have prices');
-        return Rejected('no prices');
-      })
-      .fork(console.log, (prices) => {
-        console.log('prices', prices);
+      .chain((input) => fromPromise(fetchRemainingData)(input))
+      .fork(console.error, (combinedResult) => {
+        console.log('Combined Result:', combinedResult);
       })
   );
 }
 
-export async function getRedstonePrices() {
+async function getRedstonePrices() {
   const url = 'https://api.redstone.finance/prices?provider=redstone';
 
   try {
@@ -35,9 +26,43 @@ export async function getRedstonePrices() {
     }
 
     const data = await response.json();
-    return data;
+  const firstPriceItem = extractFirstItem(data); 
+
+    return firstPriceItem;
   } catch (error) {
     console.error(error);
     return null;
   }
+}
+
+async function fetchRemainingData(prices) {
+  const firstPriceItem = extractFirstItem(prices); 
+  console.log('First Price Item:', firstPriceItem);
+
+  const url =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en';
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Failed to fetch coin data');
+    }
+
+    const data = await response.json();
+    const secondPriceItem = extractFirstItem(data); 
+    const combinedResult = { ...firstPriceItem, ...secondPriceItem }; 
+    return combinedResult;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+function extractFirstItem(obj) {
+  const keys = Object.keys(obj);
+  if (keys.length > 0) {
+    const firstKey = keys[0];
+    return { [firstKey]: obj[firstKey] };
+  }
+  return {};
 }
