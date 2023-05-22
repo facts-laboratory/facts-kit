df007433782289d08eaf320bccd5abbcb9d867ab
Luke <lukeannison@gmail.com>
2023-05-22 16:12:45 +0100

    feat: redstone and remaining fetch functions in

--- packages/el-cap-kit/src/lib/el-cap-kit.js
+++ packages/el-cap-kit/src/lib/el-cap-kit.js
@@ -1,3 +1,39 @@
+import fetch from 'node-fetch'; // Import the fetch function
+
 export function elCapKit() {
   return 'el-cap-kit';
 }
+
+export async function fetchRemainingData() {
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en';
+    
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch coin data');
+      }
+    
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+ export async function fetchRedstoneData() {
+    const url = 'https://api.redstone.finance/prices?provider=redstone';
+  
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch Redstone data');
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
--- packages/el-cap-kit/test/el-cap-kit.test.js
+++ packages/el-cap-kit/test/el-cap-kit.test.js
@@ -1,11 +1,26 @@
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 const test = suite('el-cap-kit');
+import { fetchRemainingData, fetchRedstoneData } from '../src/lib/el-cap-kit.js';
+
+
 
 test.before(async () => {});
 
 test('should not allow claiming without txId', () => {
   assert.is(1, 1);
+});
+
+test('should fetch coin data from Coingecko API', async () => {
+  const coinData = await fetchRemainingData();
+  assert.instance(coinData, Array, 'Coin data should be an array');
+  // Add additional assertions to validate the coin data if needed
+});
+
+test('should fetch data from Redstone API', async () => {
+  const redstoneData = await fetchRedstoneData();
+  assert.instance(redstoneData, Object, 'Redstone data should be an object');
+  // Add additional assertions to validate the Redstone data if needed
 });
 
 test.after(async () => {});
